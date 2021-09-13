baa6589686e2290bf763b7f080fd0d11f65fe3da
pupil <pupil@ivkhk.ee>
2021-09-13 11:16:48 +0300

    cel and fahr

--- src/fahrenheit/Fahrenheit.java
+++ src/fahrenheit/Fahrenheit.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 public class Fahrenheit {
 
     public static void main(String[] args) {
-	    double f,c;
+	    double fahr,cel;
             Scanner sc = new Scanner(System.in);
 	    System.out.println("Choose type of conversion \n 1.Fahrenheit to Celsius  \n 2.Celsius to Fahrenheit");
-            int ch = sc.nextInt();
-	    switch(ch){
+            int choice = sc.nextInt();
+	    switch(choice){
 	    case 1:  System.out.println("Enter  Fahrenheit temperature");
-                f = sc.nextDouble();
-	    	c = (f-32)*5/9;
-	    	System.out.println("Celsius temperature is = "+c);
+                fahr = sc.nextDouble();
+	    	cel = (fahr-32)*5/9;
+	    	System.out.println("Celsius temperature is = "+cel);
 		break;
 	    case 2:  System.out.println("Enter  Celsius temperature");
-                c = sc.nextDouble();
-	    	f = ((9*c)/5)+32;
-	    	System.out.println("Fahrenheit temperature is = "+f);
+                cel = sc.nextDouble();
+	    	fahr = ((9*cel)/5)+32;
+	    	System.out.println("Fahrenheit temperature is = "+fahr);
 		break;
 	    default:  System.out.println("please choose valid choice");}
     }
