21f73b16839f83701e0f0dda41ecbabd8f3f7547
pupil <pupil@ivkhk.ee>
2021-09-13 10:41:25 +0300

    Second commit

--- src/fahrenheit/Fahrenheit.java
+++ src/fahrenheit/Fahrenheit.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fahrenheit;
 
 import java.util.Scanner;
 public class Fahrenheit {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
 	    double f,c;
             Scanner sc = new Scanner(System.in);
